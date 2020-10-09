6daa0bc7ac15d3d92fcff6b86e80e7c91dcf8b58
xieyezi <1435398529@qq.com>
2020-10-09 11:06:06 +0800

    fix:prettier

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,26 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue3 + Typescript" />
+	<img alt="Vue logo" src="./assets/logo.png" />
+	<HelloWorld msg="Hello Vue3 + Typescript" />
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: "App",
-  components: {
-    HelloWorld,
-  },
-};
+	name: 'App',
+	components: {
+		HelloWorld
+	}
+}
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
 }
 </style>
